95fb7c2bc957f720cebab54e462269198491a558
aliakseis <aliaksei.sanko@gmail.com>
2020-03-04 16:49:56 +0300

    modified

--- opencv-transfer-learning.cpp
+++ opencv-transfer-learning.cpp
@@ -48,12 +48,37 @@ https://answers.opencv.org/question/191359/ml-svm-k-nn-image-recognition-example
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
 
+#include <filesystem>
+#include <map>
+#include <numeric>
+
 using namespace cv;
-using namespace std;
+//using namespace std;
+
+namespace fs = std::filesystem;
+
+auto getImagePaths(const std::string& location)
+{
+    std::map<std::string, std::vector<String>> result;
+
+    for (const auto & folder : fs::directory_iterator(location))
+    {
+        if (!folder.is_directory())
+            continue;
+        const auto name = folder.path().filename().string();
+
+        std::vector<String> fn;
+        glob(folder.path().string() + "/*.jpg", fn, true);
+        result[name] = std::move(fn);
+    }
+
+    return result;
+}
 
 int main(int argc, char** argv)
 {
-    vector<String> fn;
+    /*
+    std::vector<String> fn;
     glob("c:/data/cat-dog/*.jpg", fn, true);
     // glob() will conveniently sort names lexically, so the cats come first!
     // so we have 700 cats, 699 dogs, and split it into:
@@ -106,10 +131,10 @@ int main(int argc, char** argv)
                     train.push_back(f);
                     labels.at<float>(i - 200, 1) = 1;
                 }
-        cout << i << "\r"; // "machine learning should come with a statusbar." ;)
+        std::cout << i << "\r"; // "machine learning should come with a statusbar." ;)
     }
 
-    cout << train.size() << " " << labels.size() << " " << test.size() << endl;
+    std::cout << train.size() << " " << labels.size() << " " << test.size() << '\n';
     nn->train(train, 0, labels); // yes, that'll take a few minutes ..
     nn->save("cats.dogs.ann.yml.gz");
 
@@ -126,8 +151,159 @@ int main(int argc, char** argv)
     for (int i = 100; i < 200; i++) // 2nd hundred were dogs
         correct_dog += result.at<float>(i, 1) > result.at<float>(i, 0); // 1, true dog;
     float accuracy = (correct_cat + correct_dog) / 200;
-    cout << correct_cat << " " << correct_dog << " : " << accuracy << endl;
+    std::cout << correct_cat << " " << correct_dog << " : " << accuracy << '\n';
     // 100 99 : 0.995
+    */
+
+
+    const auto trainingPaths = getImagePaths(argv[1] + std::string("/resized_for_training"));
+    const auto testingPaths = getImagePaths(argv[1] + std::string("/resized_for_test"));
+
+    const std::string modelTxt = "c:/data/mdl/squeezenet/deploy.prototxt";
+    const std::string modelBin = "c:/data/mdl/squeezenet/squeezenet_v1.1.caffemodel";
+
+    std::vector<std::string> labelNames;
+    for (const auto& l : trainingPaths)
+    {
+        labelNames.push_back(l.first);
+    }
+
+    dnn::Net net = dnn::readNetFromCaffe(modelTxt, modelBin);
+    cv::Size inputImgSize = cv::Size(227, 227); // model was trained with this size
+
+    Mat_<int> layers(4, 1);
+    layers << 1000, 400, 100, trainingPaths.size(); // the sqeezenet pool10 layer has 1000 neurons
+
+    const char modelFileName[] = "cats.ann.yml.gz";
+
+    Ptr<ml::ANN_MLP> nn = ml::ANN_MLP::load(modelFileName);
+
+    if (!nn)
+    {
+        nn = ml::ANN_MLP::create();
+
+        nn->setLayerSizes(layers);
+        nn->setTrainMethod(ml::ANN_MLP::BACKPROP, 0.0001);
+        nn->setActivationFunction(ml::ANN_MLP::SIGMOID_SYM);
+        nn->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER + TermCriteria::EPS, 300, 0.0001));
+
+        Mat train;
+
+        const auto numLabels = std::accumulate(trainingPaths.begin(), trainingPaths.end(), 0u,
+            [](size_t n, const auto& v) { return n + v.second.size(); });
+
+        Mat labels(numLabels, trainingPaths.size(), CV_32F, 0.f);
+
+
+        int categoryIdx = 0;
+        int idx = 0;
+        for (const auto& l : trainingPaths)
+        {
+            for (const auto& v : l.second)
+            {
+                // use the dnn as a "fixed function" preprocessor (no training here)
+                Mat img = imread(v);
+                net.setInput(dnn::blobFromImage(img, 1, inputImgSize, Scalar::all(127), false));
+                Mat blob = net.forward("pool10");
+                Mat f = blob.reshape(1, 1).clone(); // now our feature has 1000 numbers
+
+                // train cat
+                train.push_back(f);
+                labels.at<float>(idx, categoryIdx) = 1; // one-hot encoded labels for our ann
+
+                ++idx;
+            }
+            ++categoryIdx;
+        }
+
+        nn->train(train, 0, labels); // yes, that'll take a few minutes ..
+        nn->save(modelFileName);
+    }
+
+    std::ofstream report("report.html");
+
+    report << R"(<!DOCTYPE html>
+<html lang="en">
+  <head>
+</head>
+  <body>
+)";
+
+    int categoryIdx = 0;
+    for (const auto& l : testingPaths)
+    {
+        report << l.first << '\n';
+
+        report << R"(
+<table>
+<thead>
+<tr>
+<th>Image</th>
+<th>Classification</th>
+<th>OK/NG</th>
+</tr>
+</thead>
+<tbody>
+)";
+
+        Mat test;
+
+        for (const auto& v : l.second)
+        {
+            // use the dnn as a "fixed function" preprocessor (no training here)
+            Mat img = imread(v);
+            net.setInput(dnn::blobFromImage(img, 1, inputImgSize, Scalar::all(127), false));
+            Mat blob = net.forward("pool10");
+            Mat f = blob.reshape(1, 1).clone(); // now our feature has 1000 numbers
+
+            test.push_back(f);
+        }
+
+        Mat result;
+        // our result array is one-hot endoded, too, which means:
+        // if pin 1 is larger than pin 0, -  it predicted a dog, else a cat.
+        nn->predict(test, result);
+
+        int correct = 0;
+        for (int i = 0; i < l.second.size(); ++i)
+        {
+            report << R"(<tr><td><a target="_blank" rel="noopener noreferrer" href=")"
+                << l.second[i]
+                << R"("><img src=")"
+                << l.second[i]
+                << R"(" alt="" style="max-width:100%;"></a></td><td>)";
+
+            float h_max = -1.e20;
+            int predicted = 0;
+            for (int label = 0; label < trainingPaths.size(); ++label)
+            {
+                const auto h = result.at<float>(i, label);
+
+                if (label)
+                    report << "<br>";
+                report << labelNames[label] << " (score = " << h << ')';
+
+                if (h > h_max)
+                {
+                    h_max = h;
+                    predicted = label;
+                }
+            }
+            const bool ok = predicted == categoryIdx;
+            if (ok)
+                ++correct;
+
+            report << "</td><td>" << (ok? "OK" : "NG") << "</td></tr>\n";
+        }
+
+        report << "</tbody></table>\n";
+
+        std::cout << "Score for " << l.first << " : " << correct << " : " << double(correct) / l.second.size() << '\n';
+
+        ++categoryIdx;
+    }
+
+    report << "</body>\n</html>";
 
     return 0;
 }
